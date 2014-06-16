9f4ebd69052c06fe25dbf1ab4766c1f52c851c9b
Neha <neha.ladva@clariontechnologies.co.in>
2014-06-16 12:43:31 +0530

    #1 issue SaveRupya application created using ruby 2.1.1 and rails 3.2.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaveRupya::Application.config.secret_token = '411962167fbb00b2b6d5ff4ed775d64ebd946a1fa4cbdf5c096c4a8016bf3f0eb6361a6b825b45c7f2703a711363b7284735a5cf78d01591bcffa6e6a91dce8d'
