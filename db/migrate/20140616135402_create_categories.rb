bd586f6b5e64de2a8be2d43992130dbfc5c8eb4f
Neha <neha.ladva@clariontechnologies.co.in>
2014-06-16 19:47:25 +0530

    #7 issue Solved the migration problem in application

---
+++ db/migrate/20140616135402_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.string :Name
+      t.boolean :Active_Inactive
+      t.string :Description
+
+      t.timestamps
+    end
+  end
+end
