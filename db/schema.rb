7a207d9a8d5d2be17339040dc0ac99f1a33e3b64
Neha <neha.ladva@clariontechnologies.co.in>
2014-06-17 11:30:52 +0530

    #9 issue remove the category model from the last commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,6 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20140616135402) do
-
-  create_table "categories", :force => true do |t|
-    t.string   "Name"
-    t.boolean  "Active_Inactive"
-    t.string   "Description"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
@@ -36,7 +27,4 @@ ActiveRecord::Schema.define(:version => 20140616135402) do
     t.datetime "updated_at",                             :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-end
+  
